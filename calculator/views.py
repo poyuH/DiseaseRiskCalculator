688d15d0de4861f8e17a9c3fe3bf312bbe654a91
Po-Yu Huang <poyu0987@gmail.com>
2019-06-17 21:51:11 +0800

    update database while submitting data from /calc, also change risk calculation as private function

--- calculator/views.py
+++ calculator/views.py
@@ -1,39 +1,71 @@
 from django.shortcuts import render
 from .forms import CalculatorForm
+from .models import CalculatorModel
+from . import globalvalues
+from user.models import CustomUser
 from . import riskcalc
+from datetime import date
 
 # Create your views here.
 def calculator(request):
     if request.method == 'POST':
         form = CalculatorForm(request.POST)
-        print(request.POST)
         if form.is_valid():
-            weight = form.cleaned_data['weight']
-            height = form.cleaned_data['height']
-            bmi = round(weight / (height**2)  * 10000, 2)
-            age = form.cleaned_data['age']
-            gender = form.cleaned_data['gender']
-            race = form.cleaned_data['race']
-            family_hx = form.cleaned_data['family_hx']
-            is_steroid = form.cleaned_data['is_steroid']
-            tc = form.cleaned_data['tc']
-            hdl = form.cleaned_data['hdl']
-            sbp = form.cleaned_data['sbp']
-            is_dm = form.cleaned_data['is_dm']
-            is_treated_htn = form.cleaned_data['is_treated_htn']
-            smoker = form.cleaned_data['smoker']
-            ascvd_risk = riskcalc.get_ascvd_risk(age, is_dm, gender, race,
-                                                 smoker, tc, hdl, sbp,
-                                                 is_treated_htn)
-            dm_risk = riskcalc.get_dm_risk(age, gender, is_treated_htn,
-                                           is_steroid, smoker, family_hx, bmi)
+            risk_dict = _calculate_risk(form)
+            # update or save data
+            if request.user.is_authenticated:
+                all_dict = {'uid': request.user, 'date': date.today()}
+                all_dict.update(risk_dict)
+                all_dict.update(form.cleaned_data)
+                obj, is_created = CalculatorModel.objects.update_or_create(
+                    date = date.today(), uid_id = request.user.id, defaults=all_dict)
 
             return render(request, 'calculator.html',
-                          {'form': form, 'bmi': bmi,
-                           'ascvd_risk': ascvd_risk, 'dm_risk': dm_risk})
+                          {'form': form, 'bmi': risk_dict['bmi'],
+                           'ascvd_risk': risk_dict['ascvd_risk'],
+                           'dm_risk': risk_dict['dm_risk']})
 
-    else:
-        form = CalculatorForm()
-    return render(request, 'calculator.html', {'form': form})
+    else: # method == 'GET'
+        gender, race, age = None, None, None
+        if request.user.is_active:
+            gender = request.user.gender
+            race = request.user.race
+            age = date.today().year - request.user.birthyear
+        elif request.user.is_authenticated:
+            try:
+                obj = CustomUser.objects.get(email=request.user.email)
+            except :
+                print('can\'t find user by email')
+            gender = obj.gender
+            race = obj.race
+            age = date.today().year - obj.birthyear
 
+        form = CalculatorForm(initial=
+                              {'gender': gender, 'race': race, 'age': age})
+        return render(request, 'calculator.html', {'form': form})
 
+def _calculate_risk(form):
+    """
+    Input: already valid form
+    return: risks in dict
+    """
+    weight = form.cleaned_data['weight']
+    height = form.cleaned_data['height']
+    bmi = round(weight / (height**2)  * 10000, 2)
+    age = form.cleaned_data['age']
+    gender = form.cleaned_data['gender']
+    race = form.cleaned_data['race']
+    family_hx = form.cleaned_data['family_hx']
+    is_steroid = form.cleaned_data['is_steroid']
+    tc = form.cleaned_data['tc']
+    hdl = form.cleaned_data['hdl']
+    sbp = form.cleaned_data['sbp']
+    is_dm = form.cleaned_data['is_dm']
+    is_treated_htn = form.cleaned_data['is_treated_htn']
+    smoker = form.cleaned_data['smoker']
+    ascvd_risk = riskcalc.get_ascvd_risk(age, is_dm, gender, race,
+                                         smoker, tc, hdl, sbp,
+                                         is_treated_htn)
+    dm_risk = riskcalc.get_dm_risk(age, gender, is_treated_htn,
+                                   is_steroid, smoker, family_hx, bmi)
+    return {'ascvd_risk': ascvd_risk, 'dm_risk': dm_risk, 'bmi': bmi}
